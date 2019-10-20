fc58f6f83c6980129fce4c081116c0778c0484c1
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 05:02:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(find_anagram) 
+    puts "Must find #{self.word} inside the following string: #{find_anagram}"
+    find_anagram.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
+  end
+  
+end 
+
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets inlest banana))
