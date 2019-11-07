00123beb50303171d490a1c3e5f268daacb539c4
Justin Miller <jayman1294@gmail.com>
2019-11-07 00:11:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  
+  array.select do |word|
+    if word.split("").sort == @word.split("").sort
+    word
+    end
+  end
+end
+
+end
