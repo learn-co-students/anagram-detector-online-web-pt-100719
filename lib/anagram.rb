0ef82d475f77012a951061092add5f3c836eab09
Derric Fragosa <fderric@gmail.com>
2019-11-08 18:36:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  
+  @@all = []
+  
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    array.each do |word|
+      puts word.split(" ")
+    end 
+  end
+ # binding.pry
+  
+end 
