8ff2fe96b9ee84e035af26ef9e3f70646ca3dc1b
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-08 03:19:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,35 @@
-# Your code goes here!
+ # Your code goes here!
+class Anagram 
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  
+  array.select do |word|
+    if word.split("").sort == @word.split("").sort
+    word
+    end
+    
+  end
+end
+
+end  
+#     if word.match %w(some_word.split == ["anagrams"])
+#     return [matches]
+#   else #if not then
+#     return []
+#     end 
+#   end
+# end 
+
+# self 
+
+#   def match(anagrams)
+#   if 
+#   else
+#   return[]
+#   end 
